307a49fe4f848931583ab1ae4c1e50a222c65780
Tim Kuminecz <tkuminecz@gmail.com>
2018-08-30 15:24:57 -0400

    update logic for overriding babel config

--- src/index.js
+++ src/index.js
@@ -5,14 +5,33 @@ const pkgPath = path.resolve(cwd, 'package.json')
 const pkg = require(pkgPath)
 const mainFile = pkg.main || 'index.ts'
 const entryPath = path.resolve(cwd, mainFile)
-const babelRcPath = path.resolve(cwd, '.babelrc')
-const babelRcExists = fs.existsSync(babelRcPath)
-const babelOpts = (!babelRcExists)
-  ? JSON.parse(fs.readFileSync(path.resolve(__dirname, './.babelrc')).toString('utf8'))
-  : {}
+const babelOpts = getBabelOpts()
 
 require('babel-register')({
   extensions: [ '.ts' ],
   ...babelOpts
 })
 module.exports = require(entryPath).default
+
+function getBabelOpts () {
+  const overridePath = path.resolve(cwd, '.babelrc')
+  const overrideExists = fs.existsSync(overridePath)
+  const overrideConfig = (overrideExists) ? loadConfig(overridePath) : {}
+
+  const localConfig = getLocalConfig()
+
+  return Object.assign({}, localConfig, overrideConfig)
+}
+
+function loadConfig (configPath) {
+  return JSON.parse(fs.readFileSync(configPath).toString('utf8'))
+}
+
+function getLocalConfig () {
+  return {
+    presets: [
+      require('babel-preset-env'),
+      require('babel-preset-typescript')
+    ]
+  }
+}
