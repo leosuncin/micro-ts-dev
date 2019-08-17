0bcc2bf860d5266fc4e43644c17d4587f3e13ea0
Jaime Leonardo Suncin Cruz <jsuncin@stbgroup.com.sv>
2019-08-17 11:52:25 -0600

    fix: Use project's bin directory
    FIX tkuminecz/micro-ts-dev#1

--- bin/micro-ts-dev.js
+++ bin/micro-ts-dev.js
@@ -3,7 +3,7 @@ const path = require('path')
 const spawn = require('child_process').spawn
 
 const entryFile = path.resolve(__dirname, '../src/index.js')
-const microPath = path.resolve(__dirname, '../node_modules/.bin/micro-dev')
+const microPath = path.resolve(process.cwd(), 'node_modules/.bin/micro-dev')
 
 spawn(microPath, [ entryFile, `--watch ${process.cwd()}` ], {
   stdio: 'inherit',
